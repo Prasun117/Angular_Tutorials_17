873526221e5e9c527b9f48a6caa8406da2e8265f
CodeSandbox <support@codesandbox.io>
2024-01-27 14:15:51 +0000

    Initial commit

---
+++ src/app/MultiComponent/multi.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-multi',
+  templateUrl: './multi.component.html',
+  styleUrl: './multi.component.scss',
+})
+export class MultiComponent {}
